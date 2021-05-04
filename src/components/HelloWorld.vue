a03fa67eaa8a75b3246d9ddb1635ea40929cbba6
Yujun Jiang <yujunmjiang@gmail.com>
2021-05-04 18:14:32 -0400

    project deployment

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <p>HOW COVID-19 HAS CHANGED THE VIDEO GAME INDUSTRY.</p>
+    <!-- <h3>By Yujun Jiang</h3> -->
+    
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+/* @font-face {
+  font-family: 'Press Start 2P', cursive;
+  src: url('/assets/fonts/PressStart2P-Regular.tff') format('truetype');
+} */
+
+h3 {
+  font-family: 'Roboto', sans-serif;
+  font-size: 1em;
+  font-weight: 600;
+  color: black;
+  margin: 40px 0 0;
+}
+p{
+  font-family: 'Roboto', sans-serif;
+  font-size: 1em;
+  font-weight: 300;
+  color: black;
+  text-align: center;
+  line-height: 1.5;
+  margin-left: 25%;
+  margin-right: 25%;
+}
+</style>
