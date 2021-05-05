6663a6345574adea5bdf1092919c8723ec4e080c
Yujun Jiang <yujunmjiang@gmail.com>
2021-05-05 16:41:29 -0400

    change to router hash mode

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router';
+import { createRouter, createWebHashHistory } from 'vue-router';
 import Home from '../views/Home.vue';
 import GetProduction from '../views/GetProduction.vue';
 import GetRevenue from '../views/GetRevenue.vue';
@@ -28,7 +28,7 @@ const routes = [
 ]
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHashHistory(),
   base:'/a-different-game/',
   routes
 })
