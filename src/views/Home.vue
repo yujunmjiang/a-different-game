8a7f91c048d224ccf530e52373d283abbe974b5c
Yujun Jiang <yujunmjiang@gmail.com>
2021-05-05 00:55:56 -0400

    update

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,15 +1,12 @@
 <template>
   <div class="home">
     <img alt="Project Logo" src="../assets/logo.png" width="220" height="230" style="margin: 5em 0">
-    <!-- <HelloWorld msg="A Different Game"/> -->
     <h1>A Different Game</h1>
     <p>HOW COVID-19 HAS CHANGED THE VIDEO GAME INDUSTRY.</p>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-// import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: 'Home',
@@ -21,11 +18,6 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-/* @font-face {
-  font-family: 'Press Start 2P', cursive;
-  src: url('/assets/fonts/PressStart2P-Regular.tff') format('truetype');
-} */
-
 h3 {
   font-family: 'Roboto', sans-serif;
   font-size: 1em;
