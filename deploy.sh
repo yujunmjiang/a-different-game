53984aabadcf73f04d51a554444fe565afb4a6b7
Yujun Jiang <yujunmjiang@gmail.com>
2021-05-04 22:19:49 -0400

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -20,6 +20,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:leeallennyc/signalsfromthefuture.git master:gh-pages
+git push -f git@github.com:yujunmjiang/a-different-game.git master:gh-pages
 
 cd -
