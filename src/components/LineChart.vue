53bcd24029a28f8a63f1703150755c123d7a6e4a
Yujun Jiang <yujunmjiang@gmail.com>
2021-05-04 21:45:28 -0400

    Initial commit

---
+++ src/components/LineChart.vue
@@ -0,0 +1,168 @@
+<template>
+  <div ref="container" class="svg-container" align="center">
+    <h1>{{ title }}</h1>
+    <!-- <svg v-if="redrawToggle === true" :width="svgWidth" :height="svgHeight * 2"> -->
+    <svg :width="svgWidth + margin.left" :height="svgHeight * 1.5">
+      <g :transform="'translate(' + margin.left + ',' + margin.top + ')'">
+        <g>
+          <path class='line-positive' :d="line" />
+        </g>
+        <XAxis
+          :xScale="xScale"
+          :yTranslate="svgHeight"
+          :id="id"
+        />
+        <YAxis
+          :yScale="yScale"
+          :xTranslate="0"
+          :id="id"
+        />
+      </g>
+    </svg>
+  </div>
+</template>
+
+<script>
+import * as d3 from 'd3';
+import XAxis from './XAxis.vue';
+import YAxis from './YAxis.vue';
+
+export default {
+  name: "LineChart",
+  components: {
+    XAxis,
+    YAxis
+  },
+  props: {
+    id: String,
+    title: String,
+    xKey: String,
+    yKey: String,
+    data: Array
+  },
+  data: () => ({
+    margin: {top: 10, left: 50, bottom: 10, right: 50 },
+    svgWidth: 0,
+    redrawToggle: true,
+    $container: null,
+    line: null
+  }),
+  mounted(){
+    this.$container = this.$refs.container
+    window.addEventListener("resize", this.onResize);
+    // this.svgWidth = document.getElementById("container").offsetWidth * 0.75;
+    this.svgWidth = this.$container.offsetWidth * 0.75;
+  },
+  updated(){
+    this.AnimateLoad();
+    this.redrawToggle = true;
+  },
+  unmounted(){
+    window.removeEventListener("resize", this.onResize)
+  },
+  methods: {
+    AnimateLoad() {
+      // d3.selectAll('rect')
+      const path = d3
+          .line()
+          .x(d => {
+            return this.xScale(d[this.xKey]);
+          })
+          .y(d => {
+            return this.yScale(d[this.yKey]);
+          })
+          .curve(d3.curveCardinal);
+      this.line = path(this.data);
+      // d3.select(this.$container).selectAll('path')
+      //   .data(this.data)
+      //   .attr('stroke-width', 1.5)
+      //   // .attr('d', d3.line())
+      //   .attr('y', this.yScale(0))
+      //   .attr('height', 0)
+      //   .transition()
+      //   .delay((d, i) => {
+      //     return i * 150;
+      //   })
+      //   .duration(1000)
+      //   .attr("y", d => {
+      //     return that.yScale(d[that.yKey]);
+      //   })
+      //   .attr("height", d => {
+      //     return that.svgHeight - that.yScale(d[that.yKey]);
+      //   })
+      //   setTimeout(() => 300); // redraw the chart 300ms after the window has been resized
+    },
+    onResize() {
+      // this.$svgWidth = document.getElementById("container").offsetWidth * 0.75;
+      this.$svgWidth = this.$container.offsetWidth * 0.75;
+      this.$data.redrawToggle = false;
+    }
+  },
+  computed: {
+    dataMax() {
+      return d3.max(this.data, d => {
+        return d[this.yKey];
+      });
+    },
+    dataMin() {
+      return d3.min(this.data, d => {
+        return d[this.yKey];
+      });
+    },
+    xScale() {
+      return d3.scaleBand()
+        .rangeRound([0, this.svgWidth])
+        // .padding(0.1)
+        .domain(
+          this.data.map(d => {
+            return d[this.xKey];
+          })
+        );
+    },
+    yScale() {
+      return d3.scaleLinear()
+        .rangeRound([this.svgHeight, 0])
+        .domain([this.dataMin > 0 ? 0 : this.dataMin, this.dataMax]);
+    },
+    // id() {
+    //   return `${this.xKey}-${this.yKey}`
+    // },
+    svgHeight() {
+      return this.svgWidth / 1.61803398875; // golden ratio
+    }
+  }
+};
+</script>
+
+<style scoped>
+.line-positive {
+  fill: none;
+  stroke: #9146ff;
+  /* stroke-width: 2px; */
+  transition: r 0.2s ease-in-out;
+}
+.line1 {
+  stroke: #9146ff;
+}
+.line2 {
+  stroke: #f92772;
+}
+.line3 {
+  stroke: #00e4c9;
+}
+.line4 {
+  stroke: #cfc468;
+}
+.line-positive:hover {
+  filter: brightness(125%)
+}
+
+.svg-container {
+  display: inline-block;
+  position: relative;
+  width: 100%;
+  padding-bottom: 1%;
+  vertical-align: top;
+  overflow: hidden;
+}
+</style>
---
+++ src/main.js
@@ -0,0 +1,12 @@
+// import Vue from 'vue'
+import { createApp } from 'vue';
+import App from './App.vue';
+import router from './router';
+
+import ElementPlus from 'element-plus';
+import 'element-plus/lib/theme-chalk/index.css';
+
+const app = createApp(App);
+app.use(router);
+app.use(ElementPlus);
+app.mount('#app');
